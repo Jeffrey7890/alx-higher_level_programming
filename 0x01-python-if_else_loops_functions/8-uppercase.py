94a759a76b836bd1d276cea52aeae2e141b28c78
Jeffrey7890 <jeffrey.ogechi@gmailcom>
2023-01-03 08:36:43 -0800

    prints upper case

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def isalpha(c):
+    return((97 <= c) and (c <= 122))
+
+
+def uppercase(str):
+    str += '\n'
+    for c in str:
+        if isalpha(ord(c)):
+            print("{:c}".format(ord(c) - 32), end='')
+        else:
+            print(c, end='')
+
+
+uppercase("best")
+uppercase("Best School 98 Battery street")
