2528c5f941d416d9ec1161a54a71e8d4ab9e7ea6
Jeffrey7890 <jeffrey.ogechi@gmailcom>
2023-01-03 14:00:14 -0800

    prints combo2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(0, 10):
-        if (i == 9 and j == 9):
-            print("{}{}".format(i, j))
-        else:
-            print("{}{}".format(i, j), end=', ')
+i, j = 0, 0
+
+for _ in range(0, 100):
+    if (i == 9 and j == 9):
+        print("{}{}".format(i,j))
+    else:
+        print("{}{}".format(i, j), end=', ')
+    j += 1
+    if j > 9:
+        i += 1
+        j = 0 
+    if i > 9:
+        i = 1
+
+
+
+
