93b68da4d5d8fc7b7f3607ecca9dcbf7d56a0e57
Jeffrey7890 <jeffrey.ogechi@gmailcom>
2023-01-07 02:25:29 -0800

    must be executable

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     length = len(my_list)
-    if length > 0:
-        for i in range(length):
+    for i in range(length):
             print("{}".format(my_list[i]))
