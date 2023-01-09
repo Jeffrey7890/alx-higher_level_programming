c79ac1e2865d19f9bd111ff8ba7b28fc2aa50caa
Jeffrey7890 <jeffrey.ogechi@gmailcom>
2023-01-09 11:53:34 -0800

    again check if list is palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,11 +1,82 @@
 #include "lists.h"
-#include "stdio.h"
+#include <stdlib.h>
+
+listint_t *add_nodeint_front(listint_t **head, int n);
+listint_t *pop_node(listint_t *head);
+int check_cycle(listint_t *list);
+
+/**
+  * is_palindrome - checked if a linked list is a palindrome
+  * @head: head of list
+  * Return: 0 if list is palindrome and 1 if not
+  */
+int is_palindrome(listint_t **head)
+{
+	listint_t *stack = NULL, *traverse = *head;
+
+	/* populate stack */
+	while (traverse != NULL)
+	{
+		add_nodeint_front(&stack, traverse->n);
+		traverse = traverse->next;
+	}
+
+	while (*head != NULL)
+	{
+		if (stack->n != (*head)->n)
+			return (0);
+		head = &(*head)->next;
+		stack = pop_node(stack);
+	}
+	return (1);
+}
+
+/**
+  * pop_node - remove the head node from the stack
+  * @head: head of list
+  * Return: listint_t structure
+  */
+listint_t *pop_node(listint_t *head)
+{
+	listint_t *temp = head;
+
+	if (head == NULL)
+		return (NULL);
+
+	head = head->next;
+	free(temp);
+	return (head);
+}
+
+/**
+  * add_nodeint_front - add node to the front of list
+  * @head: head of list
+  * @n: number to add
+  * Return: listint_t structure
+  */
+listint_t *add_nodeint_front(listint_t **head, int n)
+{
+	listint_t *new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+	return (*head);
+
+}
 
 /**
   * check_cycle - checks if linked list has a loop
   * @list: list to check
   * Return: returns 1 if loop 0 if non
   */
+
 int check_cycle(listint_t *list)
 {
 	listint_t *slow, *fast;
@@ -23,45 +94,4 @@ int check_cycle(listint_t *list)
 		fast = fast->next->next;
 	}
 	return (0);
-}
-
-
-/**
-  * is_palindrome - checks if a listint_t linked list is palindrome"
-  * @head: first node in list
-  * Return: 1 if is palindrome and 0 if not
-  */
-int is_palindrome(listint_t **head)
-{
-	listint_t *start, *end, *check;
-
-	if ((head == NULL) || (*head == NULL) || (check_cycle(*head) == 1))
-		return (0);
-
-	start = *head;
-	end = *head;
-	check = NULL;
-
-	while (end->next != NULL)
-		end = end->next;
-
-	while ((start->next != NULL))
-	{
-		while (end->next != check)
-			end = end->next;
-
-
-		head = &(*head)->next;
-
-		if (end->n != start->n)
-			return (0);
-		if ((end == start->next) || (end == start))
-			return (1);
-
-		check = end;
-		end = *head;
-		start = start->next;
-
-	}
-	return (1);
 }
