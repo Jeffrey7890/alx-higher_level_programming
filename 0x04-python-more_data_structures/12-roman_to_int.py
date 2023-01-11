3bea95a3ce9dc968656f2ad657c98cdbc9d4e25a
Jeffrey7890 <jeffrey.ogechi@gmailcom>
2023-01-11 13:46:25 -0800

    correction of checker 8

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@
 def roman_to_int(roman_string):
     if roman_string is None or type(roman_string) is not str:
         return (0)
-    ROMAN_NUMERAL_STD = {
+    ROMAN_NUMERAL = {
         'M': 1000,
         'C': 100,
         'X': 10,
@@ -15,9 +15,13 @@ def roman_to_int(roman_string):
 
     SUM = 0
     length = len(roman_string)
+    if length > 2:
+        if ROMAN_NUMERAL[roman_string[0]] < ROMAN_NUMERAL[roman_string[1]]:
+            return (None)
+
     for i in range(length):
         rchr = roman_string[i]
-        value = ROMAN_NUMERAL_STD[rchr]
+        value = ROMAN_NUMERAL[rchr]
         if rchr == 'I' and i < length - 1:
             nchr = roman_string[i + 1]
             if nchr == 'V' or nchr == 'X':
@@ -25,7 +29,7 @@ def roman_to_int(roman_string):
             elif rchr == nchr:
                 SUM = SUM + value
             i += 1
-            value = ROMAN_NUMERAL_STD[roman_string[i]]
+            value = ROMAN_NUMERAL[roman_string[i]]
         else:
             SUM = SUM + value
     return (SUM)
