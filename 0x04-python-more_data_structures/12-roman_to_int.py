9c023c8f35fdad3e2fefb77748b543c988a7794a
Jeffrey7890 <jeffrey.ogechi@gmailcom>
2023-01-11 11:59:50 -0800

    converts roman number to integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,30 @@
+#!/bin/usr/python3
+
+def roman_to_int(roman_string):
+    if roman_string is None or type(roman_string) is not str:
+        return (0)
+    ROMAN_NUMERAL_STD = {
+        'M': 1000,
+        'C': 100,
+        'X': 10,
+        'I': 1,
+        'D': 500,
+        'L': 50,
+        'V': 5
+    }
+
+    SUM = 0
+    length = len(roman_string)
+    for i in range(length):
+        rchr = roman_string[i]
+        value = ROMAN_NUMERAL_STD[rchr]
+        if rchr == 'I' and i < length - 1:
+            if value < ROMAN_NUMERAL_STD[roman_string[i + 1]]:
+                SUM = SUM - value
+            else:
+                SUM = SUM + value
+            i += 1
+            value = ROMAN_NUMERAL_STD[roman_string[i]]
+        else:
+            SUM = SUM + value
+    return (SUM)
