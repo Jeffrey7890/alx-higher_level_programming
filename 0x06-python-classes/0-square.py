130354e8f35d7f8f3b2e7206de87bf954d1d48fd
Jeffrey7890 <jeffrey.ogechi@gmailcom>
2023-01-24 14:00:38 -0800

    documentation hard

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,26 +1,5 @@
 #!/usr/python3
-
-# -*- coding: utf-8 -*-
-"""Google style docstrings.
-
-This module demonstrates documentation of an empty class in python
-this class represents a square shape
-
-Example
--------
-    $ import 0-square.py
-
-Notes
------
-    This is quite much for an empty class
-
-Attributes
-----------
-None:
-    There are no attributes men
-
-"""
-
+"""Reprensation of and empty class in python"""
 
 class Square:
     """Documentation of an empty class."""
