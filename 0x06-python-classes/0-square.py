20db01b9b4e2431af66077ac80e7aad42dd299ed
Jeffrey7890 <jeffrey.ogechi@gmailcom>
2023-01-24 13:40:04 -0800

    adding docstrings

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,14 @@
 #!/usr/python3
+
+# -*- coding: utf-8 -*-
+"""Google style docstrings.
+
+This module demonstrates documentation of an empty class in python
+this class represents a square shape,
+
+"""
+
+
 class Square:
     """Documentation of an empty class.
 
