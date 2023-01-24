fda9ab706f612527fa64bb7c1b8272956d28ea36
Jeffrey7890 <jeffrey.ogechi@gmailcom>
2023-01-24 14:18:33 -0800

    temporay square file

---
+++ 0x06-python-classes/0-square_tmp.py
@@ -0,0 +1,27 @@
+#!/usr/python3
+
+# -*- coding: utf-8 -*-
+"""Google style docstrings.
+
+This module demonstrates documentation of an empty class in python
+this class represents a square shape
+
+Example
+-------
+    $ import 0-square.py
+
+Notes
+-----
+    This is quite much for an empty class
+
+Attributes
+----------
+None:
+    There are no attributes men
+
+"""
+
+
+class Square:
+    """Documentation of an empty class."""
+    pass
