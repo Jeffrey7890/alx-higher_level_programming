55384a1992688760a65e7dc35d4c7a4d1facce5f
Jeffrey7890 <jeffrey.ogechi@gmailcom>
2023-01-25 05:39:34 -0800

    coreected pycodestyle

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """ Module for square class """
 
+
 class Square(object):
-	"""A class name square
-	used to present a class with privare attribute.
+    """A class name square
+    used to present a class with privare attribute.
 
-	Attribute
-	_________
-	__size: The size of a square is crucial for a square
+    Attribute
+    _________
+    __size: The size of a square is crucial for a square
 
-	"""
-	def __init__(self, size):
-		self.__size = size
+    """
+    def __init__(self, size):
+        self.__size = size
