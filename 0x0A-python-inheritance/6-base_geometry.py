4dabac0ad430fbb978325af21d15f628e471d805
Jeffrey7890 <jeffrey.ogechi@gmailcom>
2023-02-07 13:56:40 -0800

    base geomtery for alx

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Empty class men"""
+
+
+class BaseGeometry:
+    """Repreents an emtpy class
+        for alx"""
+
+    def area(self):
+        """Raise error for alx"""
+        raise Exception("area() is not implemented")
