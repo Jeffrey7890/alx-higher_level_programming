07df47cb18decf507fc398b4aba6a31aafbb17d8
Jeffrey7890 <jeffrey.ogechi@gmailcom>
2023-02-07 13:43:58 -0800

    chekcs if class is kind of obj

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""compares instances of class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """checks if obj is an instance
+        of a_class
+    """
+    return (isinstance(obj, a_class))
