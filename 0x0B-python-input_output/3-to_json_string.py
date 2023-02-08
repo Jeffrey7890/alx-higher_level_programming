b9e1fbda3541466734329e509a03f86eb142c454
Jeffrey7890 <jeffrey.ogechi@gmailcom>
2023-02-08 03:13:38 -0800

    represents the json of object

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""JSON demo"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """returns th JSON Representation
+        of an object
+    """
+    return (json.dumps(my_obj))
