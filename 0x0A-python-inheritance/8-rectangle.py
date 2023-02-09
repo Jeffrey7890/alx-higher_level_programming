3540447bf7e995d1f5f68c1853bf44f104667927
Jeffrey7890 <jeffrey.ogechi@gmailcom>
2023-02-08 18:47:28 -0800

    rectangle of BaseGeometry subclass

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,25 +1,15 @@
 #!/usr/bin/python3
-"""Empty class men"""
+"""Rectangle class"""
 
 
-class BaseGeometry:
-    """Repreents an emtpy class
-        for alx"""
-
-    def area(self):
-        """Raise error for alx"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """checks value"""
-        if type(value) is not int:
-            raise TypeError("{} must be an intger".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Subclass of BaseGeometry"""
+    """Subclass of BaseGeometry
+        with private attributes
+        width and height
+    """
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
