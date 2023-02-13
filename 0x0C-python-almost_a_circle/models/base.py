280da73184d194bb14c885746706456ef2d8cd0d
Jeffrey7890 <d3v3loperpy@gmail.com>
2023-02-13 22:55:31 +0100

    pycoding style is annoying

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -11,6 +11,7 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """initialization men please ease on the doc"""
         if id is not None:
             self.id = id
         else:
