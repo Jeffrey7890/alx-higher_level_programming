f68639cb440755e3f146b359114d8748e8566668
Jeffrey7890 <d3v3loperpy@gmail.com>
2024-07-05 23:35:01 -0700

    should not run when imported

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,12 +2,13 @@
 
 """ How to use th urllib module in python """
 
-import urllib.request
-
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    html = response.read()
-    string = f"""Body response:
+if __name__ == "__main__":
+    import urllib.request
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        string = f"""Body response:
     - type: {type(html)}
     - content: {html}
     - utf8 content: {html.decode()}"""
-    print(string)
+        print(string)
