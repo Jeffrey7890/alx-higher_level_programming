003f7e86c1591c53c1762ce508907f53f388e696
root <=>
2024-07-30 06:55:56 -0700

    get status codes

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,9 +4,13 @@ const request = require('request');
 
 const URL = process.argv[2];
 
+if (!URL){
+    process.exit(1);
+}
 request(URL, (error, response) => {
   if (error) {
-    return;
+    console.error(error);
+    process.exit(1);
   }
   console.log('code: ', response.statusCode);
 });
