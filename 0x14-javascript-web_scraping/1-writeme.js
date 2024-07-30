b1146a3b6ad90efb19a1865678d3188781e50fe1
root <=>
2024-07-30 06:35:40 -0700

    write to file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+const fileContent = process.argv[3];
+
+fs.writeFile(filePath, fileContent, 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+});
