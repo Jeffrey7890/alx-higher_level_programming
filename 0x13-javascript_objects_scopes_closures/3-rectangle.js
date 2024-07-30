db5aef299dd89140d46e899e1badec7795c07619
root <=>
2024-07-30 16:54:48 -0700

    number 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -10,7 +10,7 @@ class Rectangle {
   print () {
     for (let i = 0; i < this.height; i++) {
       const temp = [];
-      for (let j = 0; i < this.width; j++) temp.push('X');
+      for (let j = 0; j < this.width; j++) temp.push('X');
       console.log(`${temp.join('')}`);
     }
   }
