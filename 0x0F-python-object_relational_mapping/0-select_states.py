f9d0b741bbfa855a02349fa74e27a93033203832
jeffrey7890 <d3v3loperpy@gmail.com>
2024-08-07 01:12:49 +0000

    object relational mapping

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" selects states from the database htbn_0e_0_usa """
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(
+        user=username,
+        password=password,
+        database=database,
+        host='localhost',
+        port=3306
+        )
+    c = db.cursor()
+    c.execute("""SELECT * FROM states ORDER BY id ASC""")
+    result = c.fetchall()
+    for row in result:
+        print(row)
