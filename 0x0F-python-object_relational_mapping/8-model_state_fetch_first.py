77bd7e9006e465022d80b650f480b91419dc7eef
jeffrey7890 <d3v3loperpy@gmail.com>
2024-08-07 04:29:46 +0000

    number 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-
-""" get states first data """
+"""prints the first State object from the database hbtn_0e_6_usa"""
 
 if __name__ == "__main__":
-    from model_state import Base, State
-    from sqlalchemy.orm import sessionmaker
-    from sqlalchemy import create_engine
 
     import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
 
-    argv1 = sys.argv[1]
-    argv2 = sys.argv[2]
-    argv3 = sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
-    string = 'mysql+mysqldb://{}:{}@localhost/{}'.format(argv1, argv2, argv3)
-    engine = create_engine(string, pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    for row in session.query(State).filter(State.id == 0):
-        print("{id}: {name}".format(id=row.id, name=row.name))
+    session = Session(engine)
+    first = session.query(State).order_by(State.id).first()
+    if first:
+        print("{}: {}".format(first.id, first.name))
+    else:
+        print("Nothing")
+    session.close()
